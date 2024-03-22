d2f2c48a174d768154cc8657437bee1dc8459c93
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2024-03-22 08:11:45 -0500

    adjusting events api to transform responses properly

--- libs/react-store/src/events/events-api.ts
+++ libs/react-store/src/events/events-api.ts
@@ -8,7 +8,6 @@ import {
   EventModelSchema,
 } from '@uplift-lunch-n-learn/models';
 import { EventTag } from '../api-tags';
-import { format, parseISO } from 'date-fns';
 
 export const eventsApi = reactStoreApi.injectEndpoints({
   endpoints: (build) => ({
@@ -37,15 +36,13 @@ export const eventsApi = reactStoreApi.injectEndpoints({
       query: (id) => ({
         url: `events/${id}`,
       }),
+      transformResponse: (result) => EventModelSchema.parseAsync(result),
     }),
     createEvent: build.mutation<EventModel, Partial<EventModel>>({
       invalidatesTags: [EventTag],
       query: (args) => ({
         url: 'events',
-        body: {
-          ...args,
-          date: args.date ? args.date.toISOString() : args.date,
-        },
+        body: args,
         method: 'POST',
       }),
     }),
@@ -57,10 +54,7 @@ export const eventsApi = reactStoreApi.injectEndpoints({
         error ? [] : [{ type: EventTag, id: arg.id }, EventTag],
       query: (args) => ({
         url: `events/${args.id}`,
-        body: {
-          ...args,
-          date: args.date ? args.date.toISOString() : args.date,
-        },
+        body: args,
         method: 'PUT',
       }),
     }),
