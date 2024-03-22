f9b14a0b994e4e52c2f426d2bd8aa88c32b0f150
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2024-03-22 11:51:36 -0500

    adding a bit of a script to the basics page

--- apps/react-web/src/basics/BasicsPage.tsx
+++ apps/react-web/src/basics/BasicsPage.tsx
@@ -1,17 +1,28 @@
 import { FC } from 'react';
 import { ColumnFlexbox } from '@uplift-lunch-n-learn/react-ui';
-import { Typography } from '@mui/material';
+import { Switch, Typography } from '@mui/material';
 
 export const BasicsPage: FC = () => {
   // Use State
+  // - Simple boolean with Switch
+  // - Save settings in state
 
   // Use Memo
+  // - Show object being created each time with a useEffect
 
   // Use Effect
+  // - Load settings using effect
+  // - Load events using effect & settings
+
+  // Use Callback
+  // - Refetching data from server on button click
 
   // Use Reducer
+  // - Consolidate fetch properties using a reducer
 
   // Use Context
+  // - Consolidate reducer state into a context
+
   return (
     <ColumnFlexbox>
       <Typography variant={'h3'}>React Basics</Typography>
